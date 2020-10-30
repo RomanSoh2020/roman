96fb0ce289bc676cce8a452db556129f7da9d921
Roman <romashoh5@mail.ru>
2020-10-31 00:28:31 +0200

    lesson 12

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert(5);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ' ');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последний просмотренных фильмов? ", ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt("Один из последний просмотренных фильмов? ", ''),
+      d = prompt("Один из последний просмотренных фильмов? ", '');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
